2db61b0dc91ad5267457615bb245deac4727d435
Sebastianbv05 <132632589+Sebastianbv05@users.noreply.github.com>
2023-06-29 12:37:23 -0300

    Delete Braian/Recomendacion directory

--- Braian/Recomendacion/conexionlibreria.php
+++
@@ -1,8 +0,0 @@
-<?php 
-        $usuario="root";
-        $clave="";
-        $servidor="127.0.0.1";
-        $base="libreria2";
-
-        $conexion=mysqli_connect($servidor,$usuario,$clave,$base);
-?>
