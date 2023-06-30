ce6397ad0a9337a3eab970f9e9cf47cf645f037f
Sebastianbv05 <132632589+Sebastianbv05@users.noreply.github.com>
2023-06-30 12:59:49 -0300

    Delete Braian/Biblioteca directory

--- Braian/Biblioteca/categoria/cerrar_sesion.php
+++
@@ -1,6 +0,0 @@
-<?php
-     //si deceo serrar la sesion temporalmente
-     session_start();
-     session_destroy();
-     header("location: ../index/index.html");
-?>
--- Braian/Biblioteca/categoria/conexion_be.php
+++
@@ -1,4 +0,0 @@
-<?php
-//conectar con el servidor
-$conexion=mysqli_connect("localhost","root","","index");
-?>
